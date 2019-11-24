e82824c44c5ea11396fad538ddc4aa923da9e874
Moyosore Ajayi <ajayimy@gmail.com>
2019-11-24 13:50:04 +0100

    contact details and list in angular

--- src/app/contacts/contact-details/contact-details.component.ts
+++ src/app/contacts/contact-details/contact-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ContactService } from './../contact.service';
+import { Contact } from './../contact';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-contact-details',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  contact: Contact
+
+  @Input()
+  createHandler: Function
+  @Input()
+  updateHandler: Function
+  @Input()
+  deleteHandler: Function
+
+  constructor(private contactService: ContactService) { }
 
   ngOnInit() {
   }
 
+    createContact(contact: Contact) {
+      this.contactService.createContact(contact).then((newContact: Contact) => {
+        this.createHandler(newContact)
+      })
+    }
+
+    updateContact(contact: Contact): void {
+      this.contactService.updateContact(contact).then((updatedContact: Contact) => {
+        this.updateHandler(updatedContact)
+      })
+    }
+
+    deleteContact(contact: Contact) {
+      this.contactService.deleteContact(contactId).then((deletedContactId: String) => {
+        this.deleteHandler(deletedContactId)
+      })
+    }
 }
